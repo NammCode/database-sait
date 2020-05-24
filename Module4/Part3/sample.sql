08e0b0b62b6f9b4a19365b1e280c50fc01e85538
NamDell <namkhanh.nguyen@edu.sait.ca>
2020-05-24 16:10:03 -0600

    Module 4 Part 3

--- Module4/Part3/sample.sql
+++ Module4/Part3/sample.sql
@@ -82,8 +82,93 @@ JOIN orderitems oi ON (b.isbn = oi.isbn)
 ORDER BY 1,2,3;
 
 -- slide 18: Overview
--- Tradional Join using =
+-- Tradional Join using = operator
 -- NATURAL JOIN based on common columns
+-- JOIN .. USING when have column in common.
+-- JOIN .. ON when different name
+
+-- slide 19 - 20: Self-Join
+-- Used to link a table itself 
+-- Traditional JOIN  or JOIN .. ON
+-- slide 21: List all customers reffered by Leila (1003) and Meshia (1006)
+SELECT c.firstname "Customer", rb.firstname "Ref by"
+FROM customers c, customers rb 
+WHERE c.referred = rb.customer# 
+AND rb.customer# IN (1003, 1006)
+ORDER BY 1,2;
+
+-- slide 22: List all customers who have reffered other customers.
+SELECT c.lastname || ' ' || c.firstname "Customer Name", rb.lastname "Refer to" 
+FROM customers c, customers rb 
+WHERE c.customer# = rb.referred
+ORDER BY 1, 2;
+
+-- slide 23: Can  use JOIN .. ON for itself JOIN.
+SELECT c.lastname || ' ' || c.firstname "Customer Name", rb.lastname "Refer to" 
+FROM customers c JOIN customers rb 
+ON (c.customer# = rb.referred)
+ORDER BY 1, 2;
+
+-- slide 26 outer Join, Tradional Method
+-- Show customers and all thier order. Include customers with no orders.
+-- o.customer#(+) means generate a null value row to match customers that have no orders
+SELECT firstname, order# 
+FROM customers c, orders o 
+WHERE c.customer# = o.customer#(+)
+ORDER BY 1,2;
+
+-- slide 27
+SELECT firstname, order#
+FROM customers LEFT OUTER JOIN orders USING (customer#)
+ORDER BY 1, 2;
+  
+-- slide 29
+-- Includes row on BOTH sides that do not have a matching inner join connections.
+-- Show all customers and books. Include customers that have not ordered any books
+-- and books that have not been ordered by any customer.
+SELECT firstname, lastname, title 
+FROM customers FULL OUTER JOIN orders USING (customer#) 
+FULL OUTER JOIN orderitems USING (order#) 
+FULL OUTER JOIN books USING (isbn) 
+ORDER BY 1, 2;
+
+-- slide 30 - 32: Non-Equality Joins
+-- In WHERE clause, use any comparison operator other than the equal sign.
+-- The gift associated with a book will be based on the book's price
+SELECT title, retail, gift 
+FROM books b, promotion p 
+WHERE b.retail BETWEEN p.minretail AND p.maxretail;
+
+-- slide 33:
+-- In FROM clause, use JOIN .. ON keywords with a non-equivalent condition.
+SELECT title, retail, gift 
+FROM books b JOIN promotion p 
+ON (b.retail BETWEEN p.minretail AND p.maxretail);
+
+-- slide 34 - 35: UNION
+-- Used to combine the result of two or more SELECT statements
+SELECT 'Author: ' || fname || ' ' || lname "Author Name and Contact" 
+FROM author 
+UNION 
+SELECT 'Publisher: ' || contact || ' - Company: ' || name 
+FROM publisher;
+
+-- slide 36: INTERSECT (giao nhau) 
+-- Returns only the rows included in the result of both queries
+SELECT customer# FROM customers 
+INTERSECT 
+SELECT customer# FROM orders;
+
+-- slide 37: MINUS
+-- Subtracts the second query's results if they're also returned in the first query's results
+-- Find all customers that do not have an order
+-- set (ALL customers) - set (Customers with orders) ==> set (customers without orders)
+SELECT customer# FROM customers 
+MINUS 
+SELECT customer# FROM orders;
+
+
+
 
 
 
